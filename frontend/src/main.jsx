59ae435bf87e1d073f69d0326b9fc0b730d2689f
Yassinos <yassinecastro@proton.me>
2024-09-13 15:42:09 +0100

    setting up env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import Store from "./redux/Store.js";
+import Store from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
